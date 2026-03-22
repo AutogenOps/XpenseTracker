c2338e49283886c5782bcb4e42771540d3d19f87
AI Engineer <96913572+AutogenOps@users.noreply.github.com>
2026-03-22 21:47:38 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-
-// Initialize Redux store
-const store = createStore(rootReducer);
-
-export default store;
